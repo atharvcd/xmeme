10ab6c5d5c4f84036649a19ec8fc38b846b1c327
atharvcd <atharvcd@gmail.com>
2021-02-14 20:45:04 +0530

    xmeme v1

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -49,10 +49,10 @@ const Home = () => {
     loadAllRecentMemes();
   }, [memeCreated,memeUpdated,memeDeleted]);
   return (
-    <div className = "row" style = {{display : "flex", overflow : "hidden", height : "auto", border : "1px solid cyan", margin : "1% 3% 1% 2%"}}>
-       <div style = {{width : "100%", border : "1px solid red", margin : "0.5% auto 3% auto"}}>
+    <div className = "row" style = {{display : "flex", overflow : "hidden", height : "auto", margin : "1% 3% 1% 2%"}}>
+       <div style = {{width : "100%", margin : "0.5% auto 3% auto"}}>
          <div style = {{marginBottom : "2%"}}>
-          <p style = {{fontFamily : "Sofia", color : "#ffffff", fontSize : "3vw"}}>Top 10 Trending Memes</p>
+          <p style = {{fontFamily : "Sofia", color : "#ffffff", fontSize : "3vw"}}>&nbsp; Top 10 Trending Memes</p>
          </div>
          <div style = {{display : "flex", margin : "0 auto", padding : "2%", backgroundColor : "#1f1f1f"}}>
           <div style = {{width : "25%"}}><TrendingMemes memeList = {recentMemes}/></div>
@@ -61,13 +61,13 @@ const Home = () => {
           <div style = {{width : "25%"}}><TrendingMemes memeList = {recentMemes}/></div>
         </div>
       </div>
-      <div className = "col-md-8" style = {{float : "left", padding : "0 2% 0 0%", border : "1px solid red"}}>
-        <div className = "row" style = {{display : "flex",alignContent : "center", margin : "0.8% auto", border : "1px solid red"}}>
+      <div className = "col-md-8" style = {{float : "left", padding : "0 2% 0 0%"}}>
+        <div className = "row" style = {{display : "flex",alignContent : "center", margin : "0.8% auto"}}>
             <div className = "col-md-5" style = {{ textAlign : "left",fontSize : "3vw" ,fontFamily : "Sofia", color : "#ffffff", margin : "1.3% 1%"}}>Recent Memes</div>
             <div className = "col-md-3" style = {{textAlign : "left",paddingTop : "1%", width : "50%", marginTop : "1.2%"}}><AddMeme handleCreatedStatus = {itemCreatedStatus} created = {memeCreated}/></div>
         </div>
         
-        <div className="scrollbar scrollbar-primary  mx-auto" style = {{border : "1px solid red", height : "92vh", overflowY : "scroll", overflowX : "hidden", padding : "2.5% 2.5%", backgroundColor : "#1f1f1f", filter : "brightness(100%)"}}>
+        <div className="scrollbar scrollbar-primary  mx-auto" style = {{height : "92vh", overflowY : "scroll", overflowX : "hidden", padding : "2.5% 2.5%", backgroundColor : "#1f1f1f", filter : "brightness(100%)"}}>
           <Grid container direction="column" alignItems = "center">
             <Grid item container>
               <Grid item xs={12} sm={12}>
@@ -77,9 +77,9 @@ const Home = () => {
           </Grid>
         </div>
       </div>
-      <div className = "col-md-4" style = {{textAlign : "right", width : "100%",border : "1px solid red", height : "100%", padding : "0.5%"}}>
-        <div style = {{textAlign : "left",fontSize : "3vw", fontFamily : "Sofia", color : "#ffffff", border : "1px solid black", margin : "1.5% 1%"}}>Trending Creators</div>
-        <div className="scrollbar scrollbar-primary  mt-4 mx-auto" style = {{backgroundColor : "#2c2c2c", opacity : "1" ,border : "1px solid red", height : "92vh", overflowY : "scroll", overflowX : "hidden", padding : "5% 5%"}}>
+      <div className = "col-md-4" style = {{textAlign : "right", width : "100%",height : "100%", padding : "0.5%"}}>
+        <div style = {{textAlign : "left",fontSize : "3vw", fontFamily : "Sofia", color : "#ffffff", margin : "1.5% 1%"}}>Trending Creators</div>
+        <div className="scrollbar scrollbar-primary  mt-4 mx-auto" style = {{backgroundColor : "#2c2c2c", opacity : "1", height : "92vh", overflowY : "scroll", overflowX : "hidden", padding : "5% 5%"}}>
           <MemerList memeList = {recentMemes}/>
         </div>
       </div>
